85c86394d4c1d7ecaa940a83d13824dde2410436
Egitva <9026479948e@gmail.com>
2026-03-30 21:16:57 +0500

    3003

--- Test/test_button.py
+++ Test/test_button.py
@@ -1,4 +1,5 @@
 #нажатие кнопки
+import allure
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,10 +12,17 @@ from selenium.webdriver.support import expected_conditions as EC
 # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 def test_button(driver_init):
-    driver_init.get("https://perm.medsi.ru/")
-    driver_init.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    found_button = driver_init.find_element(By.CSS_SELECTOR, "a.btn.btn-yellow.for-utm-link")
-    found_button.click()
-    text_element = driver_init.find_element(By.XPATH, "//h1[contains(text(), 'Записаться на прием')]")
-    text = text_element.text
-    assert text == "Записаться на прием"
+    with allure.step("Открытие сайта Медси"):
+        driver_init.get("https://perm.medsi.ru/")
+
+    with allure.step("Скролим страницу вниз"):
+        driver_init.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    with allure.step("Нажимаем на кнопку 'Записаться на прием'"):
+        found_button = driver_init.find_element(By.CSS_SELECTOR, "a.btn.btn-yellow.for-utm-link")
+        found_button.click()
+
+    with allure.step("Проверяем что на экране есть текст 'Записаться на приём'"):
+        text_element = driver_init.find_element(By.XPATH, "//h1[contains(text(), 'Записаться на прием')]")
+        text = text_element.text
+        assert text == "Записаться на прием"
